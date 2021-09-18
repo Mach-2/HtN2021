456ef6033b8ce2a34345cc6977fe933e2893c704
Madison Chapel <46977570+Mach-2@users.noreply.github.com>
2021-09-18 14:36:33 -0500

    Update citations.py

--- citations.py
+++ citations.py
@@ -2,55 +2,50 @@
 A function to create citations for Books or Ebooks. Different forms of the citation will be created depending on which parameters have arguments passed to them. Each citation is compliant with APA7. 
 """
 
-def citing_books(authors_first_name=False, authors_middle_name=False, authors_last_name=False, book_title=False, publisher=False, publishing_year=False,
-                 DOI=False, alternate_description=False):
+import numpy as np
+from author_names import *
+
+
+def citing_books(book_title=False, publisher=False, publishing_year=False,
+                 DOI=False, alternate_description=False, **names):
     # Author name should be LastName, first initial, middle initial 
-    if (authors_last_name):
-        if authors_first_name:
-            first_initial = authors_first_name[0:1]
-            final_author_name = authors_last_name + ", " + first_initial + "."
-        if authors_middle_name:
-            middle_initial = authors_middle_name[0:1]
-            final_author_name = authors_last_name + ", " + first_initial + ". " + middle_initial + "."
-        else:
-            final_author_name = authors_last_name+ "."
+    authors_list = author_names(**names)
 
     # Begin generating final citation
     # Author, date, and title missing
-    if authors_last_name == False and publishing_year == False and book_title == False:
+    if authors_list == False and publishing_year == False and book_title == False:
         if alternate_description:
             reference = "[" + alternate_description + "]. (n.d.), " + publisher
         else:
             return "Not enough information to generate a citation"
     # Date and title are missing    
-    elif (publishing_year == False and book_title == False):
+    elif publishing_year == False and book_title == False:
         if alternate_description:
-            reference = final_author_name + ". (n.d.). [" + alternate_description + "]. " + publisher
+            reference = authors_list + ". (n.d.). [" + alternate_description + "]. " + publisher
         else:
             return "Not enough information to generate a citation"
             # Author and date are missing
-    elif (authors_last_name == False and publishing_year == False):
+    elif (authors_list == False and publishing_year == False):
         reference = book_title + ". (n.d.). " + publisher
         # Source is missing
     elif (publisher == False):
         return "Not enough information to generate a citation"
     # Title is missing 
     elif (book_title == False):
-        reference = final_author_name + ".(" + publishing_year + "). [" + alternate_description + "]. " + publisher
+        reference = authors_list + ".(" + publishing_year + "). [" + alternate_description + "]. " + publisher
     # Date is missing
     elif (publishing_year == False):
-        reference = final_author_name + ". (n.d.). " + book_title + ". " + publisher + "."
+        reference = authors_list+ " (n.d.). " + book_title + ". " + publisher + "."
     # Author is missing
-    elif (authors_last_name == False):
+    elif authors_list == False:
         reference = book_title + ". (" + publishing_year + "). " + publisher + "."
-
     # Final reference if all info present
     else:
-        reference = final_author_name + " (" + publishing_year + ")." + book_title + ". " + publisher + "."
-
+        reference = authors_list + " (" + publishing_year + "). " + book_title + ". " + publisher + "."
     # DOI, optional
     if DOI:
         reference = reference + " " + DOI
-
     return reference
+
+
 
